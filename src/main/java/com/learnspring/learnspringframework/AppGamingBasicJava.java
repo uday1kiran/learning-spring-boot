2729aacdafed7c24c44635ed787cb8fcb8ae1fcf
uday <uday.kiran.reddy@quest.com>
2023-08-28 01:31:40 +0530

    Tight coupled example 2

--- src/main/java/com/learnspring/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/learnspring/learnspringframework/AppGamingBasicJava.java
@@ -2,12 +2,13 @@ package com.learnspring.learnspringframework;
 
 import com.learnspring.learnspringframework.game.GameRunner;
 import com.learnspring.learnspringframework.game.MarioGame;
-import org.springframework.boot.SpringApplication;
+import com.learnspring.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        var marioGame = new MarioGame();// ctrl+1 will give option to create class.sub package: game;
-        var gameRunner = new GameRunner(marioGame);//package: game
+        //var marioGame = new MarioGame();// ctrl+1 will give option to create class.sub package: game;
+        var superContraGame = new SuperContraGame();
+        var gameRunner = new GameRunner(superContraGame);//package: game
         gameRunner.run();//ctrl+1
     }
 }
---
+++ src/main/java/com/learnspring/learnspringframework/game/SuperContraGame.java
@@ -0,0 +1,16 @@
+package com.learnspring.learnspringframework.game;
+
+public class SuperContraGame {
+    public void up(){
+        System.out.println("up");
+    }
+    public void down(){
+        System.out.println("Sit down");
+    }
+    public void left(){
+        System.out.println("go back");
+    }
+    public void right(){
+        System.out.println("Shoot a bullet");
+    }
+}
